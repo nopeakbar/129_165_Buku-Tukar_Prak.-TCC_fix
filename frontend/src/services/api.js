5876308e63624a58971b3eea37b0280b742654e1
nopeakbar <nopeakbar@gmail.com>
2025-05-31 18:22:11 +0700

    ubah url fe ke be dan sebaliknya

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+// const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://exchange-book-dot-xenon-axe-450704-n3.uc.r.appspot.com/';
 
 // Create axios instance
 const api = axios.create({
